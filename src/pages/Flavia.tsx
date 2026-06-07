032ed068d1eb6fa0e2243cb237c32058cedb0c1f
DavidAlexKelly <dakelly16@gmail.com>
2026-06-07 21:49:26 +0100

    tweaks new

--- src/pages/Flavia.tsx
+++ src/pages/Flavia.tsx
@@ -6,21 +6,35 @@ export function Flavia() {
 
     `Part of me hated it. I was so foreign to the concept of feeling anything at all it was overwhelming. I felt the logical sides of me bend and crack. My decision-making crumble. The feeling made it hard to eat, to sleep, to live. Every part of me wanted every part of you. It was more than love. I wanted to be you. This emotion is what led me to say the things I regret saying when we were high.`,
 
-    `If anything, please do not take what I said as that I only care about you in a physical way, because I don't. It's not that I want to sleep with you, it's that I want so wholly to be close to you, to be beside you, to see all aspects of you, to feel and see all of you. I want to hold you so tightly with nothing between us and have the whole world fade away. What I really want is what I had with you sleeping beside me in your garden as I watched planes cut through the sky and I was so happy I wanted to die.  This isn't me being a typical man. It's pain. The fact you do not feel the same way felt like my chest being crushed in that moment. Years of apathetic wandering only to find something that I was so close to not finding by pure chance, and then feel like that thing wasn't what I thought. That was all I could think about.`,
+    `If anything, please do not take what I said as that I only care about you in a physical way, because I don't. It's not that I want to sleep with you, it's that I want so wholly to be close to you, to be beside you, to see all aspects of you, to feel and see all of you. I want to hold you so tightly with nothing between us and have the whole world fade away. What I really want is what I had with you sleeping beside me in your garden as I watched planes cut through the sky and I was so happy I wanted to die. This isn't me being a typical man. It's pain. The fact you do not feel the same way felt like my chest being crushed in that moment. Years of apathetic wandering only to find something that I was so close to not finding by pure chance, and then feel like that thing wasn't what I thought. That was all I could think about.`,
 
-    `But as I sit here and focus on the pain and really observe it, it isn't that at all. Looking closer, I see now that it's in reality the fear of losing you. That one day you will find someone who you feel that connection with more than me, and I'll be alone again. In hindsight, my reaction has damned me to my worst fear. So surprised with my luck, I dropped what I had in the process. I am naturally self sabotaging and though I am much better than I used to be, still mentally ill and im sorry you had to see that.`,
+    `But as I sit here and focus on the pain and really observe it, it isn't that at all. Looking closer, I see now that it's in reality the fear of losing you. That one day you will find someone who you feel that connection with more than me, and I'll be alone again. In hindsight, my reaction has damned me to my worst fear. So surprised with my luck, I dropped what I had in the process. I am naturally self sabotaging and though I am much better than I used to be, still mentally ill and I'm sorry you had to see that.`,
 
-    `Flavia, you really are one of a kind. I hope we travel the world and do all the dumb stuff we said we would do. And if not I will cherish the memories we had and forever be grateful that I had such a chance encounter with such a fantastic person.`,
+    `I know this is all a bit much and I know you may read this and run but its how I feel and Flavia, you really are one of a kind. I hope we travel the world and do all the dumb stuff we said we would do. And if not I will cherish the memories we had and forever be grateful that I had such a chance encounter with such a fantastic person.`,
 
     `Now and forever Flavia I love you.`
   ];
 
- 
   const loveLines = [
-
     "I love how funny you are.",
     "I love how you play along with my bits with no hesitation.",
     "I love your outlook on life.",
+    "I love your unrelenting spontaneity.",
+    "I love your curiosity towards all things and people.",
+    "I love how your bridge makes my bridge look lame.",
+    "I love the way you lie.",
+    "I love your confidence in yourself and your thoughts.",
+    "I love how you'll deny you're confident at all.",
+    "I love how our coincidences are old news.",
+    "I love how your name sounds when I say it.",
+    "I love how my name sounds when you say it.",
+    "I love your chimney sweep British accent.",
+    "I love your chimney sweep British look.",
+    "I love how damn cool you look when you smoke.",
+    "I love how naturally you hold a glass of wine.",
+    "I love your past and how it's as fucked up as mine.",
+    "I love how those big brown eyes swallow me whole.",
+    "I love your tattoos and how easily you get them.",
     "I love the way you sound in all three languages.",
     "I love the way you smile.",
     "I love the way you laugh.",
